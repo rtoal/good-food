bee441eec7ee22952549640a3eb6b9c8013cbf27
Ray Toal <rtoal@lmu.edu>
2022-12-11 16:53:20 -0800

    Get ready to show details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [meals, setMeals] = useState([]);
+  const [term, setTerm] = useState("");
+  const [mealId, setMealId] = useState(null);
+
+  // page can be "results", "details"
+  const [page, setPage] = useState("results");
+
+  useEffect(() => {
+    const searchUrl = "https://www.themealdb.com/api/json/v1/1/filter.php";
+    fetch(`${searchUrl}?c=${encodeURIComponent(term)}`).then((r) =>
+      r.json().then((r) => {
+        setMeals(r?.meals);
+        setPage("results");
+      })
+    );
+  }, [term]);
+
   return (
     <div className="App">
       <h1>Good Food</h1>
+      <SearchForm action={setTerm} />
+      {page === "results" ? (
+        <SearchResults meals={meals} setPage={setPage} setMealId={setMealId} />
+      ) : (
+        <MealDetail id={mealId} />
+      )}
     </div>
   );
 }
 
-export default App;
+function SearchForm({ action }) {
+  const [content, setContent] = useState("");
+  function submit(e) {
+    e.preventDefault();
+    action(content);
+    setContent("");
+  }
+  return (
+    <form onSubmit={submit}>
+      Search by category{" "}
+      <input value={content} onChange={(e) => setContent(e.target.value)} />
+    </form>
+  );
+}
+
+function SearchResults({ meals, setPage, setMealId }) {
+  function details(mealId) {
+    setPage("details");
+    setMealId(mealId);
+  }
+  return (
+    <div>
+      {meals &&
+        meals.map((meal) => (
+          <div key={meal.idMeal}>
+            <h2 onClick={(e) => details(meal.idMeal)}>{meal.strMeal}</h2>
+            <div>
+              <img width="300" src={meal.strMealThumb} alt="yummy" />
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function MealDetail({ id }) {
+  return <div>Showing details for id={id}</div>;
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBsXkjDQEpKSffRK8X2wJt8Q-MfGpqxs4",
+  authDomain: "good-food-e632c.firebaseapp.com",
+  projectId: "good-food-e632c",
+  storageBucket: "good-food-e632c.appspot.com",
+  messagingSenderId: "155255288474",
+  appId: "1:155255288474:web:854ef914a1748bdf45a523",
+};
+
+export const app = initializeApp(firebaseConfig);
